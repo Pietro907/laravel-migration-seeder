cb0ade9ffaa918957f4816228a63271cc358f42a
Pietro907 <pietrop.puri@gmail.com>
2023-10-30 14:28:30 +0100

    add initial page

---
+++ resources/views/layout/app.blade.php
@@ -0,0 +1,54 @@
+<?php
+
+/* 
+
+Creiamo una tabella trains con relativa Migration e Model.
+Ogni treno dovrà avere almeno:
+
+- Azienda
+- Stazione di partenza
+- Stazione di arrivo
+- Orario di partenza
+- Orario di arrivo
+- Codice Treno
+- Numero Carrozze
+- In orario
+- Cancellato
+ 
+
+> Oltre ai campi impostati da laravel di default in tutte le migrazioni :occhiolino:
+
+Inserite qualche dato usando un seeder tramite Faker PHP per popolare la vostra tabella.
+
+Quando il db é pronto e i dati inseriti, 
+create un Controller per mostrare nella home page tutti i treni che sono in partenza
+dalla data odierna.
+
+*/
+
+?>
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+
+    
+
+</body>
+
+</html>
