0f66f55ca7da81e87bee98071f35c28dd5438774
Pietro907 <pietrop.puri@gmail.com>
2023-10-30 15:58:00 +0100

    add create db laravel-migration seeder & create a columns

---
+++ database/migrations/2023_10_30_142710_create_trains_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('Azienda', 50);
+            $table->string('Stazione di partenza', 100);
+            $table->string('Stazione di arrivo', 100);
+            $table->string('Orario di partenza');
+            $table->string('Orario di arrivo');
+            $table->string('Codice treno');
+            $table->smallInteger('Numero carrozza');
+            $table->boolean('In orario');
+            $table->boolean('Cancellato');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trains');
+    }
+};
--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -47,7 +47,23 @@ dalla data odierna.
 
 <body>
 
-    
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-2">
+                <div class="card">
+                    <p>Azienda</p>
+                    <p>Stazione di partenza</p>
+                    <p>Stazione di arrivo</p>
+                    <p>Orario di partenza</p>
+                    <p>Orario di arrivo</p>
+                    <p>Codice Treno</p>
+                    <p>Numero Carrozze</p>
+                    <p>In orario</p>
+                    <p>Cancellato</p>
+                </div>
+            </div>
+        </div>
+    </div>    
 
 </body>
 
